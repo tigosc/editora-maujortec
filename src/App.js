3b42541bc66d398345cbc8469a8b57280ea6c1df
tigosc <tigosc@gmail.com>
2026-02-23 22:42:44 -0300

    editora-maujortec

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+// import './App.css';
+// import React, { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="topo">
+        <h1 className="logo"></h1>
+        <ul>
+          <li><a href="#">Navegação</a></li>
+        </ul>
       </header>
-    </div>
+      <main className="principal">
+        <h2>Últimos Lançamentos</h2>
+        <div className="card">Card</div>
+      </main>
+      <footer className="rodape">
+        <p>Conteúdos cedidos pela Editora Novatec &#8212; Copyright 2026</p>
+      </footer>
+    </>
   );
 }
 
